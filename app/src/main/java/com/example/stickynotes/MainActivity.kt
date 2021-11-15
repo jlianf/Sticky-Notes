7e010e0334fedeede9b576085f85fa083602bbe6
Your Name <you@example.com>
2021-11-15 15:15:26 +0700

    Update MainActivity.kt part 2

--- app/src/main/java/com/example/stickynotes/MainActivity.kt
+++ app/src/main/java/com/example/stickynotes/MainActivity.kt
@@ -46,7 +46,7 @@ class MainActivity : AppCompatActivity(), onClickItemListener {
         modeGrid()
 
         //get Data Catatan
-        getNote(requestShow = , false)
+        getNote(REQUEST_SHOW, false)
 
     }
 
@@ -65,11 +65,66 @@ class MainActivity : AppCompatActivity(), onClickItemListener {
             private fun doInBackground(var p0: Void?): List<ModelNote>?{
                 return NoteDatabase.getInstance(this@MainActivity).noteDao().allnote as List<ModelNote>?
             }
-
+            private fun onPostExecute(notes: List<ModelNote>) {
+                super.onPostExecute(notes)
+                if (requestCode == REQUEST_SHOW) {
+                    modelNoteList.addAll(notes)
+                    noteAdapter?.notifyDataSetChanged()
+                } else if (requestCode == REQUEST_ADD){
+                    modelNoteList.add(0, notes[0])
+                    noteAdapter?.notifyItemInserted(0)
+                    rvListNote.smoothScrollToPosition(0)
+                } else if (requestCode == REQUEST_UPDATE){
+                    modelNoteList.removeAt(onClickPosition)
+                    if (deleteNote){
+                        noteAdapter?.notifyItemRemoved(onClickPosition)
+                    } else {
+                        modelNoteList.add(onClickPosition, notes[onClickPosition])
+                        noteAdapter?.notifyItemChanged(onClickPosition)
+                    }
+                }
+            }
         }
+        GetNoteAsyncTask().execute()
 
     }
 
+    public override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?){
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_ADD && resultCode == RESULT_OK){
+            getNote(REQUEST_ADD, false)
+        } else if (requestCode == REQUEST_UPDATE && resultCode == RESULT_OK){
+            if (data != null) {
+                getNote(REQUEST_UPDATE, data.getBooleanExtra("NoteDelete", false))
+            }
+        }
+    }
 
+    override fun onClick(modelNote: ModelNote, position: Int){
+        onClickPosition = position
+        val intent = Intent(this, CreateNoteActivity:: class.java)
+        intent.putExtra("EXTRA", true)
+        intent.putExtra("EXTRA_NOTE", modelNote)
+        startActivityForResult(intent, REQUEST_UPDATE)
+    }
+
+    companion object{
+        private const val REQUEST_ADD = 1
+        private const val REQUEST_UPDATE = 2
+        private const val REQUEST_SHOW = 3
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId){
+            R.id.listView -> modeList()
+            R.id.gridView -> modeGrid()
+        }
+        return super.onOptionsItemSelected(item)
+    }
 
 }
